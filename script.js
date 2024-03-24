44374cdafa829706ddb15251d846cd13aecdca1a
chicken-muggets <160193048+chicken-muggets@users.noreply.github.com>
2024-03-24 15:15:12 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
+talked = 0
 w = "Type your first message"
 while( 1 == 1) {
     w= prompt(w,"respond");
-    
+    talked ++
+    getElementById("talkedamount").innerHTML = "you have talked to yourself {talked} amount"
 }
