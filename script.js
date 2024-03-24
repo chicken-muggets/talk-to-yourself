f3ebc6ec6fe4ff85455a2c437c741a0ccf39c014
chicken-muggets <160193048+chicken-muggets@users.noreply.github.com>
2024-03-24 15:22:16 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 talked = 0
 w = "Type your first message"
-while( 1 == 1) {
+while(1 === 1) {
     w= prompt(w,"respond");
     talked ++
     getElementById("talkedamount").innerHTML = "you have talked to yourself {talked} amount"
